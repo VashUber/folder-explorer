3c256581937b52384f86944db44373cab7b8af00
VashUber <71492611+VashUber@users.noreply.github.com>
2023-05-09 01:01:09 +0300

    feature: initialize

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="p-4">
+      <div className="text-2xl text-center">Folder explorer</div>
+    </div>
+  );
+}
---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export interface FileI {
+  name: string;
+  type: "file";
+}
+
+export interface FolderI {
+  name: string;
+  type: "folder";
+  data: (FolderI | FileI)[];
+}
+
+export type ExplorerT = (FolderI | FileI)[];
