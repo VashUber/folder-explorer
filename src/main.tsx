16438739e8ccb638578fb4f4f3233f474c9b821a
VashUber <71492611+VashUber@users.noreply.github.com>
2023-05-09 14:53:56 +0300

    nevermind

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,4 @@ import ReactDOM from "react-dom/client";
 import App from "~/App.tsx";
 import "~/styles/tailwind.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
